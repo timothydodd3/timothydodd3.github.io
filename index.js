305fa9fcc2a2a054726a603599ba2d12e9f17f96
Timothy Dodd <77984950+timothydodd3@users.noreply.github.com>
2025-06-18 15:00:38 -0600

    Website Update: June 2025
    Corrected Footer. Added more text. Updated background color.

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-// For reference: https://www.typescriptlang.org/docs/handbook/typescript-in-5-minutes.html
-var welcomeMsg = "Welcome to Tim's Webpage!";
-var welcomeHead = document.createElement("h1");
+// The Title Page
+document.title = "Welcome to My Site!";
+var welcomeMsg = "Hi There!";
+var welcomeHead = document.createElement("header");
 welcomeHead.textContent = welcomeMsg;
+var introMsg = document.createElement("h3");
+introMsg.textContent = "Welcome to my site!";
+var homeBody = document.createElement("p");
+homeBody.textContent = "My Name is Tim, and I'm a Software Engineer specializing in C++." +
+    " This is my first personal Typescript project & eventually" +
+    " will become my professional online portfolio.";
+var disclaimer = document.createElement("p");
+disclaimer.textContent = "This site is still under construction, " +
+    " Feel free to check whenever for updates!";
 document.body.append(welcomeHead);
+document.body.append(introMsg);
+document.body.append(homeBody);
+document.body.append(disclaimer);
--- index.ts
+++ index.ts
@@ -1,9 +1,23 @@
-// For reference: https://www.typescriptlang.org/docs/handbook/typescript-in-5-minutes.html
- 
-const welcomeMsg: string = "Welcome to Tim's Webpage!";
+// The Title Page
+document.title = "Welcome to My Site!";
 
-const welcomeHead = document.createElement("h1") as HTMLHeadElement;
+const welcomeMsg: string = "Hi There!";
+const welcomeHead = document.createElement("header") as HTMLHeadElement;
 welcomeHead.textContent = welcomeMsg;
 
-document.body.append(welcomeHead);
+const introMsg = document.createElement("h3") as HTMLHeadingElement;
+introMsg.textContent = "Welcome to my site!"
 
+const homeBody = document.createElement("p") as HTMLParagraphElement;
+homeBody.textContent = "My Name is Tim, and I'm a Software Engineer specializing in C++." + 
+" This is my first personal Typescript project & eventually" + 
+" will become my professional online portfolio.";
+
+const disclaimer = document.createElement("p") as HTMLParagraphElement;
+disclaimer.textContent = "This site is still under construction, " +
+" Feel free to check whenever for updates!";
+
+document.body.append(welcomeHead);
+document.body.append(introMsg);
+document.body.append(homeBody);
+document.body.append(disclaimer);
